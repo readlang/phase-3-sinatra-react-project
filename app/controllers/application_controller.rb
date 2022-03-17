f928e13f33a8fd2bf619ff8692193bece91556ee
Read Langworthy <rlangworthy@gmail.com>
2022-03-17 14:48:31 -0700

    update login and signup routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,44 +1,41 @@
+require "pry"
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
+  # These are Login actions, which will live in App
   get "/login/:user/:pass" do
-    #this could be a post instead of a get
+    #this could be a post instead of a get - everything could be a post
     user = params[:user]
     pass = params[:pass]
     if User.find_by(username: user) == nil
-      {message: "Username not found", username: user }.to_json
+      {message: "Username not found", username: user, id: 0 }.to_json
     else
       if pass == User.find_by(username: user).password
         {message: "Authorized", username: user, id: User.find_by(username: user).id }.to_json
       else
-        {message: "Not authorized", username: user}.to_json
+        {message: "Incorrect password", username: user, id: 0 }.to_json
       end 
     end    
   end
 
-  get "/rooms" do
-    rooms = Room.all
-    rooms.to_json
-  end
-
-  get "/room/:id/messages" do
-    #check if room is locked to new users?
-    room_id = params[:id].to_i
-    messages = Message.where(room_id: room_id)
-    messages.to_json
-  end
-  
   post "/signup" do
     if User.find_by(username: params[:username]) != nil
-      {message: "Username is already taken", username: params[:username]}.to_json
+      {message: "Username is already taken", username: params[:username], id: 0 }.to_json
     else
       new_user = User.create(
         username: params[:username],
         password: params[:password]
       )
-      new_user.to_json
+      {message: "Authorized", username: new_user[:username], id: new_user[:id] }.to_json
     end
+  end
 
+
+  # These are Room actions, which live in RoomSelect
+  get "/rooms" do
+    rooms = Room.all
+    rooms.to_json
   end
 
   post "/create_room" do
@@ -53,6 +50,15 @@ class ApplicationController < Sinatra::Base
     end
   end
 
+
+  # These are Message actions, which live in MessageRoom
+  get "/room/:id/messages" do
+    #check if room is locked to new users?
+    room_id = params[:id].to_i
+    messages = Message.where(room_id: room_id)
+    messages.to_json
+  end
+  
   post "/create_message" do
     new_message = Message.create(
       user_id: params[:user_id],
@@ -68,7 +74,8 @@ class ApplicationController < Sinatra::Base
     message.to_json
   end
 
-  # frontend will need some way to check for a new message from the other members of chat room
-  # or a way to "push" the message from the backend
-
 end
+
+  # frontend will need some way to check for a new message from the other members of chat room
+  # seems like it should just check (get request) every 5 seconds or something
+  # or a way to "push" the message from the backend - not sure if this is possible
